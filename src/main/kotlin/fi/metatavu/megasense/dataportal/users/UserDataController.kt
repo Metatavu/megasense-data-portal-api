7ee91be6ada1fa52e4bddcc1ccef819448bad141
platonovsimeon <simeon.platonov@gmail.com>
2020-10-23 08:08:23 +0300

    Refactoring the Users API

---
+++ src/main/kotlin/fi/metatavu/megasense/dataportal/users/UserDataController.kt
@@ -0,0 +1,20 @@
+package fi.metatavu.megasense.dataportal.users
+
+import java.io.File
+import java.util.*
+import javax.enterprise.context.ApplicationScoped
+
+/**
+ * A controller for user data
+ */
+@ApplicationScoped
+class UserDataController {
+    /**
+     * Extracts user data
+     *
+     * @param userId user id
+     */
+    fun extractUserData (userId: UUID): File {
+
+    }
+}
